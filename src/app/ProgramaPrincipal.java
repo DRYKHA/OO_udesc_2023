7f993f25b377d39f6858459c8c3f86a8e816bf68
DRYKHA <adrianemms1@hotmail.com>
2024-05-06 21:02:54 -0300

    Classe Pessoa agora com atributos públicos

--- src/app/ProgramaPrincipal.java
+++ src/app/ProgramaPrincipal.java
@@ -1,14 +1,38 @@
+package domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+public class Pessoa {
+    public String nome;
+    public String endereço;
+    public LocalDate nascimento;
+    public BigDecimal salario;
+
+}
 package app;
 
 import domain.Pessoa;
 
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class ProgramaPrincipal {
     public static void main(String[] args) {
-      Pessoa fulano = new Pessoa();
+        Pessoa fulano = new Pessoa();
 
-      System.out.println(fulano.nome);
+        fulano.nome = "Fulano da Silva";
+        fulano.endereço = "Rua dos programadores, 42";
+        fulano.nascimento = LocalDate.of(1990, 10,  13);
+        fulano.salario = new BigDecimal("1000.99");
 
-          // A linha de baixo acusa erro pois não consegue acessar nome
-        System.out.println(fulano.nome);
+        System.out.println("Nome......: " + fulano.nome);
+        System.out.println("Endereço..: " + fulano.endereço);
+        System.out.println("Nascimento.: " + fulano.nascimento);
+        System.out.println ("Salário....: " + fulano.salario");
     }
 }
+
+
